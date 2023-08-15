a514949b547f13427585c0415da1fa10cb4fdd3e
Ashutosh Raj <ashutoshraj2000@gmail.com>
2023-08-15 17:18:32 +0530

    first commit

---
+++ src/component/Basics/menuAPI.js
@@ -0,0 +1,94 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "₹30",
+      description:
+      "MAGGI noodles (70 grams) Chopped vegetables ( capsicum , cabbage, carrot etc ) chopped onion, chopped tomato, chopped green chilly, tablespoons all purpose flour, salt, red chili powder, MAGGI masala.Cheese cubes.",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "Aloo Pakora",
+      category: "evening",
+      price: "₹50",
+      description:
+        "Aloo Pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy gram flour batter. Make this awesome vegan potato snack.",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "Corn",
+      category: "breakfast",
+      price: "₹20",
+      description:
+        "2 Cup sweet corn kernels (frozen/fresh) ¼ Cup + 2 tablespoon corn flour (corn starch) 2 tablespoon all-purpose flour (maida) 1 teaspoon black pepper powder (kali mirch) oil to deep-fry.",
+    },
+    {
+      id: 4,
+      image: "images/chola.jpg",
+      name: "Chola",
+      category: "lunch",
+      price: "₹50",
+      description:
+        "Dried chickpea - 1 cup, soaked overnight Red onion - 1 and 1/2 medium size, thinly sliced 3 garlic cloves, finely chopped 1/2 inch ginger, finely chopped 1 red tomato, diced or sliced Tomato puree - 1 cup 1 tablepsoon oil",
+    },
+    {
+      id: 5,
+      image: "images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "₹80",
+      description:
+        "2 cup all purpose flour, 100 gm processed cheese, 100 ml tomato ketchup, 1 tomato, 1 capsicum (green pepper), 2 onion 1 teaspoon oregano.",
+    },
+    {
+      id: 6,
+      image: "images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "₹200",
+      description:
+        "Chicken curry: chicken pieces cooked in onion-tomato gravy, Aloo gobi: potato and cauliflower cooked in a spicy gravy, Dal fry: pigeon pea lentil (toor dal) cooked with onion, tomatoes and aromatic spices, Carrot halwa: Our all time favorite dessert, Boondi raita: whisked yogurt mixed with boondi or tiny fried gram flour balls, Rice and roti",
+    },
+    {
+      id: 7,
+      image: "images/vegthali.jpg",
+      name: "Veg Thali",
+      category: "dinner",
+      price: "₹150",
+      description:
+        "Aloo jeera, paneer bhurji gravy, dal tadka, boondi raita, mango pickle, jeera rice, phulka",
+    },
+    {
+      id: 8,
+      image: "images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "₹50",
+      description:
+        "Flour: gram flour, almond flour, whole wheat flour, Milk: milk powder, milk solids, khoya (a semi-solid dairy product made by slowly boiling milk until it thickens), Sugar/ Jaggery, Ghee/ coconut oil, Nuts and seeds - almonds, pistachios, cashews, raisins, coconut, melon seeds, etc, Legumes/ Grains - moong dal, rice, quinoa, broken wheat, Flavor enhancers - cardamom, saffron, nutmeg, rose water, rose petals",
+    },
+    {
+      id: 9,
+      image: "images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "₹80",
+      description:
+        "Rasam rice is a nutritious south indian one pot meal recipe which is made from combining rice and rasam together. It is also known as rasam sadham in south indian homes.",
+    },
+    {
+      id: 10,
+      image: "images/samosa.jpg",
+      name: "Samasa",
+      category: "evening",
+      price: "₹50",
+      description:
+        "Flaky and crunchy fried samosa are one of the most popular street food snack in North Indian cuisine. They feature a pastry-like crust but are filled with savory potatoes and peas for a hearty, delicious snack.",
+    },
+  ];
+  export default Menu;
